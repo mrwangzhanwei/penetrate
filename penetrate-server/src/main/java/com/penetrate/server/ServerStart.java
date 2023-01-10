372aa1bf7607c9f86cf9c87de0d9e5afac3ccee5
wangzhanwei <mrwangzhanwei@163.com>
2023-01-10 15:43:42 +0800

    增加logback日志

--- penetrate-server/src/main/java/com/penetrate/server/ServerStart.java
+++ penetrate-server/src/main/java/com/penetrate/server/ServerStart.java
@@ -1,8 +1,14 @@
 package com.penetrate.server;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ServerStart {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerStart.class);
     public static void main(String[] args) throws Exception {
+        LOGGER.info("哈哈");
         if (null != args && args.length == 2) {
             int visitorPort = Integer.parseInt(args[1]);
             int serverPort = Integer.parseInt(args[0]);
